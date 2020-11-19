0e4679727ca94ae8abcef22961531ba6255cb2b0
Lazar Lazarević <lazarlazarevic@Lazars-MacBook-Pro-2018.local>
2020-11-20 00:35:07 +0100

    added change in first section

---
+++ js/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+    "host": "smtp.gmail.com",
+    "username": "domacin.aplikacija@gmail.com",
+    "to": "zeljko.stefany@gmail.com",
+    "password": "fyvXew-sasdin-ryxro2",
+    "subject": "[FORM] - forma sa sajta domacin"
+}
---
+++ js/sendMail.js
@@ -0,0 +1,38 @@
+function sendEmail(message) {
+	Email.send({
+	Host: conf.host,
+	Username : conf.username,
+	Password : conf.password,
+	To : conf.to,
+	From : conf.username,
+	Subject : conf.subject,
+	Body : message,
+	}).then(
+		message => console.log(message)
+    ).catch(
+        e => console.log('error', e)
+
+    )
+}
+
+$('form').submit(function (event) {
+    event.preventDefault();
+    console.log(event)
+    const mail = {
+        name: this.name.value,
+        city: this.city.value,
+        email: this.email.value,
+        phone: this.phone.value,
+        message: this.message.value,
+    }
+    const body = `
+    <p>ime i prezime: ${mail.name}</p>
+    <p>grad: ${mail.city}</p>
+    <p>email: ${mail.email}</p>
+    <p>telefon: ${mail.phone}</p>
+    <hr>
+    <p>poruka: ${mail.message}</p>
+    `
+    console.log(mail)
+    sendEmail(message)
+})
